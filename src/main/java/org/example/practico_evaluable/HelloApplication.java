8867b122270f205a1214758b950852862aeceb18
Oscar Luque Porca <oscarluqueporca@gmail.com>
2024-11-04 12:09:15 +0100

    Practico_Evaluable

--- src/main/java/org/example/practico_evaluable/HelloApplication.java
+++ src/main/java/org/example/practico_evaluable/HelloApplication.java
@@ -5,7 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Clase principal de la aplicación JavaFX.
+ */
 public class HelloApplication extends Application {
+
+    /**
+     * Método de inicio de la aplicación JavaFX.
+     *
+     * @param stage El escenario principal de la aplicación.
+     * @throws Exception Si hay un error al cargar el archivo FXML.
+     */
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
@@ -15,6 +25,11 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    /**
+     * Método principal que lanza la aplicación.
+     *
+     * @param args Los argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
         launch();
     }
