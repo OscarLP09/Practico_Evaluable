dd71104f73488ea62df84e92d3d1d4b645882fc3
Oscar Luque Porca <oscarluqueporca@gmail.com>
2024-11-04 11:17:32 +0100

    Practico_Evaluable

--- src/main/java/org/example/practico_evaluable/HelloApplication.java
+++ src/main/java/org/example/practico_evaluable/HelloApplication.java
@@ -2,28 +2,18 @@ package org.example.practico_evaluable;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        Parent root = loadFXML(); // Llama al método para cargar el FXML
-        Scene scene = new Scene(root); // Crea la escena con el root
-        stage.setScene(scene); // Establece la escena en el escenario
-        stage.setTitle("Mi Aplicación JavaFX");
-        stage.show(); // Muestra el escenario
-    }
-
-    private Parent loadFXML() throws IOException {
+    public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        return fxmlLoader.load();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Mi Aplicación JavaFX");
+        stage.show();
     }
-
-
 
     public static void main(String[] args) {
         launch();
